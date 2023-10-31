d506cece201f592a2b30a1d702ee364b60bba3ed
Daniel Cormier <dcormier@users.noreply.github.com>
2023-10-31 12:07:03 -0400

    Added `Path::name()` and `::indexes_field()` constructors, and `::append()` method. Better docs.

--- src/path/mod.rs
+++ src/path/mod.rs
@@ -45,49 +45,83 @@ use crate::{
 ///
 /// # Examples
 ///
+/// The safest way to construct a [`Path`] is a [parse] it from a string.
+/// ```
+/// use dynamodb_expression::path::Path;
+/// # use pretty_assertions::assert_eq;
+///
+/// let path: Path = "foo".parse().unwrap();
+/// let path: Path = "foo[3]".parse().unwrap();
+/// let path: Path = "foo[3][7]".parse().unwrap();
+/// let path: Path = "foo[3][7].bar".parse().unwrap();
+/// let path: Path = "bar.baz".parse().unwrap();
+/// let path: Path = "baz[0].foo".parse().unwrap();
+/// ```
+///
+/// This makes the assumption that each path element is separated by a
+/// period (`.`). For example, the path `foo.bar` gets treated as if `bar` is a
+/// sub-attribute of `foo`. However, `.` is also a [valid character in an
+/// attribute name][3].
+///
+/// If you have an attribute name with a `.` in it, and need it to not be
+/// treated as a separator, you can construct the [`Path`] a few different ways.
+/// Take an attribute named `attr.name` as an example. If you were to [parse]
+/// it, like above, it would be treated as a path with an attribute, `attr`, and
+/// a sub-attribute, `name`.
+///
+/// Here are some ways you can correctly construct such a [`Path`].
+/// ```
+/// use dynamodb_expression::path::{Element, Path};
+/// # use pretty_assertions::assert_eq;
+/// #
+/// // If a top-level attribute name:
+/// let path = Path::name("attr.name");
+///
+/// // If the top-level attribute, `foo`, has a sub-attribute named `attr.name`:
+/// let path = Path::from_iter([
+///     Element::name("foo"),
+///     Element::name("attr.name"),
+/// ]);
+///
+/// // If top-level attribute `foo`, item 3 (i.e., `foo[3]`) has a sub-attribute
+/// // named `attr.name`:
+/// let path = Path::from_iter([
+///     Element::indexed_field("foo", 3),
+///     Element::name("attr.name"),
+/// ]);
+///
+/// // If top-level attribute `foo`, item 3, sub-item 7 (i.e., `foo[3][7]`) has
+/// // an attribute named `attr.name`:
+/// let path = Path::from_iter([
+///     Element::indexed_field("foo", [3, 7]),
+///     Element::name("attr.name"),
+/// ]);
+/// ```
+///
 /// Each of these are ways to create a [`Path`] instance for `foo[3][7].bar[2].baz`.
 /// ```
 /// use dynamodb_expression::{path::{Element, Path}};
 /// # use pretty_assertions::assert_eq;
 /// #
-/// # let expected: Path = [
+/// # let expected = Path::from_iter([
 /// #     Element::indexed_field("foo", [3, 7]),
 /// #     Element::indexed_field("bar", 2),
 /// #     Element::name("baz"),
-/// # ]
-/// # .into_iter()
-/// # .collect();
+/// # ]);
 ///
 /// // A `Path` can be parsed from a string
 /// let path: Path = "foo[3][7].bar[2].baz".parse().unwrap();
 /// # assert_eq!(expected, path);
 ///
-/// // `Path` implements `FromIterator` for items that are `Into<Element>`.
-/// let path = Path::from_iter([("foo", vec![3, 7]), ("bar", vec![2]), ("baz", vec![])]);
+/// // `Path` implements `FromIterator` for items that `Element`s.
+/// let path = Path::from_iter([
+///     Element::indexed_field("foo", [3, 7]),
+///     Element::indexed_field("bar", 2),
+///     Element::name("baz"),
+/// ]);
 /// # assert_eq!(expected, path);
 ///
 /// // Of course, that means you can `.collect()` into a `Path`.
-/// let path: Path = [("foo", vec![3, 7]), ("bar", vec![2]), ("baz", vec![])]
-///     .into_iter()
-///     .collect();
-/// # assert_eq!(expected, path);
-///
-/// // `Element` can be converted into from string/index tuples. Where the
-/// // string is the attribute name. In this case, an "index" is an array,
-/// // slice, `Vec` of, or a single `u32`.
-/// // It's smart about it, though. If if there's one or zero indexes it'll do
-/// // the right thing. This helps when you're chaining iterator adapters and
-/// // the results are values with inconsistent numbers of indexes.
-/// let path: Path = [
-///     Element::from(("foo", vec![3, 7])),
-///     Element::from(("bar", vec![2])),
-///     Element::from(("baz", vec![])),
-/// ]
-/// .into_iter()
-/// .collect();
-/// # assert_eq!(expected, path);
-///
-/// // You can also explicitly construct `Element`s.
 /// let path: Path = [
 ///     Element::indexed_field("foo", [3, 7]),
 ///     Element::indexed_field("bar", 2),
@@ -96,6 +130,33 @@ use crate::{
 /// .into_iter()
 /// .collect();
 /// # assert_eq!(expected, path);
+///
+/// // `Element` can be converted into from string/index tuples. Where the
+/// // string is the attribute name. In this case, an "index" is an array,
+/// // slice, `Vec` of, or a single `u32`.
+/// //
+/// // It's smart about it, though. If if there's one or zero indexes it'll do
+/// // the right thing. This helps when you're chaining iterator adapters and
+/// // the results are values with inconsistent numbers of indexes.
+/// let path = Path::from_iter(
+///     [
+///         ("foo", vec![3, 7]),
+///         ("bar", vec![2]),
+///         ("baz", vec![]),
+///     ]
+///     .map(Element::from),
+/// );
+/// # assert_eq!(expected, path);
+///
+///
+/// // `Path` implements `FromIterator` for items that are `Into<Element>`.
+/// // So, the above example can be simplified.
+/// let path = Path::from_iter([
+///     ("foo", vec![3, 7]),
+///     ("bar", vec![2]),
+///     ("baz", vec![]),
+/// ]);
+/// # assert_eq!(expected, path);
 /// ```
 ///
 /// If you have a document path where an [attribute name includes a period][3]
@@ -103,16 +164,16 @@ use crate::{
 /// ```
 /// # use dynamodb_expression::path::{Element, Path};
 /// # use pretty_assertions::assert_eq;
-/// // If the field name is `foo.bar`:
+/// // If the attribute name is `foo.bar`:
 /// let path = Path::from(Element::name("foo.bar"));
 /// # assert_eq!(Path::from_iter([Element::name("foo.bar")]), path);
 ///
-/// // If the item at `foo[3]` has a field named `bar.baz`:
+/// // If the item at `foo[3]` has an attribute named `bar.baz`:
 /// let path = Path::from_iter([Element::indexed_field("foo", 3), Element::name("bar.baz")]);
 /// # assert_eq!("foo[3].bar.baz", path.to_string());
 /// ```
 ///
-/// `Name` and `Path` can be converted between each other.
+/// [`Name`] and [`Path`] can be converted between each other.
 /// ```
 /// use dynamodb_expression::path::{Element, Name, Path};
 ///
@@ -121,7 +182,7 @@ use crate::{
 /// let path = Path::from(name);
 /// assert_eq!(Path::from(Element::name("foo")), path);
 ///
-/// // A `Path` consisting of a single, unindexed field can be converted into a `Name`.
+/// // A `Path` consisting of a single, unindexed attribute can be converted into a `Name`.
 /// let path: Path = "foo".parse().unwrap();
 /// let name = Name::try_from(path).unwrap();
 /// assert_eq!(Name::from("foo"), name);
@@ -140,10 +201,57 @@ use crate::{
 /// [1]: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.Attributes.html#Expressions.Attributes.NestedElements.DocumentPathExamples
 /// [2]: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.ExpressionAttributeNames.html
 /// [3]: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.Attributes.html#Expressions.Attributes.TopLevelAttributes
+/// [parse]: str::parse
 /// [`Expression`]: crate::expression::Expression
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Path {
-    pub elements: Vec<Element>,
+    pub(crate) elements: Vec<Element>,
+}
+
+impl Path {
+    /// Constructs a [`Path`] for a single attribute name (with no indexes or
+    /// sub-attributes). If you have a attribute name with one or more indexes,
+    /// use [`Path::indexed_field()`].
+    ///
+    /// [`Path::indexed_field()`]: Self::indexed_field
+    pub fn name<T>(name: T) -> Self
+    where
+        T: Into<Name>,
+    {
+        Self {
+            elements: vec![Element::name(name)],
+        }
+    }
+
+    /// Constructs a [`Path`] for a single attribute name (with no indexes or
+    /// sub-attributes). If you have a attribute name with no indexes, you can
+    /// pass an empty array, or use [`Path::name()`].
+    ///
+    /// [`Path::name()`]: Self::name
+    pub fn indexed_field<N, I>(name: N, indexes: I) -> Self
+    where
+        N: Into<Name>,
+        I: Indexes,
+    {
+        Self {
+            elements: vec![Element::indexed_field(name, indexes)],
+        }
+    }
+
+    /// Appends the another [`Path`] to the end of this one.
+    ///
+    /// ```
+    /// use dynamodb_expression::path::Path;
+    /// # use pretty_assertions::assert_eq;
+    ///
+    /// let mut path:Path="foo".parse().unwrap();
+    /// let sub_path:Path="bar".parse().unwrap();
+    /// path.append(sub_path);
+    /// assert_eq!("foo.bar".parse::<Path>().unwrap(), path);
+    /// ```
+    pub fn append(&mut self, mut other: Path) {
+        self.elements.append(&mut other.elements);
+    }
 }
 
 /// Methods relating to building condition and filter expressions.
